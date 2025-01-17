5b9d04378887281a2ec913c074737ecca39873e5
Nguyễn Mạnh Hùng <hungnm@boai.vn>
2025-01-17 16:14:06 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div className="App-content">
+        <h1>Chào mừng bạn đến với React!</h1>
+        <p>Học React thật thú vị!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/functions.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+};
+
+sum2digits = (a, b) => a + b;
